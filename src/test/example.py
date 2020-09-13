368250030dab90e89a64775a0074ddab129fd57e
jvendrow <jvendrow@gmail.com>
2020-09-13 01:08:54 -0700

    adding functions

---
+++ src/test/example.py
@@ -0,0 +1,55 @@
+import sys
+sys.path.insert(1, '../')
+
+from ndl import Wtd_NNetwork
+from ndl import NetDictLearner
+#import ndl.utils as utils
+from ndl import utils
+
+
+def learn_and_reconstruct():
+
+    G = Wtd_NNetwork()
+    G.load_add_wtd_edges("Caltech36.txt", use_genfromtxt=True)
+
+    NDL = NetDictLearner(G=G, n_components=25, MCMC_iterations=20)
+    NDL.train_dict(verbose=True)
+    G_recons = NDL.reconstruct(recons_iter=5000)
+
+    acc = utils.recons_accuracy(G, G_recons)
+    print(acc)
+
+def additive_corruption():
+
+    G = Wtd_NNetwork()
+    G.load_add_wtd_edges("Caltech36.txt", use_genfromtxt=True)
+
+    G_corrupt, edges_changed = utils.corrupt(G, parameter=0.01, noise_type="ER")
+
+    NDL_corrupt = NetDictLearner(G=G_corrupt, n_components=25, MCMC_iterations=50)
+
+    NDL_corrupt.train_dict()
+    G_corrupt_recons = NDL_corrupt.reconstruct(recons_iter=1000, return_weighted=True)
+    F, T, auc, fpr, tpr, thresholds = utils.auc_roc(G, G_corrupt, G_corrupt_recons, noise_type="positive")
+    print(auc)
+
+def subtractive_corruption():
+
+    G = Wtd_NNetwork()
+    G.load_add_wtd_edges("Caltech36.txt", use_genfromtxt=True)
+
+    G_corrupt, edges_changed = utils.corrupt(G, parameter=0.1, noise_type="negative")
+    print(len(G.get_edges()))
+    print(len(G_corrupt.get_edges()))
+
+    NDL_corrupt = NetDictLearner(G=G_corrupt, n_components=25, MCMC_iterations=2)
+
+    NDL_corrupt.train_dict()
+    G_corrupt_recons = NDL_corrupt.reconstruct(recons_iter=5000, return_weighted=True)
+    F, T, auc, fpr, tpr, thresholds = utils.auc_roc(G, G_corrupt, G_corrupt_recons, noise_type="negative")
+    print(auc)
+
+
+#learn_and_reconstruct()
+#additive_corruption()
+subtractive_corruption()
